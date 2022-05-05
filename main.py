7186dfa6fee8302bd88318e45db833656e70726c
Ripp <bartekzadrozny@wp.pl>
2022-05-05 14:03:30 +0200

    added gui

---
+++ main.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import sys
+
+import quantumHashing
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+
+class TableInputDelegate(QItemDelegate):
+    def createEditor(self, parent, option, index):
+        w = QLineEdit(parent)
+        w.setValidator(QRegularExpressionValidator(QRegularExpression("\d*")))
+        return w
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        self.quantumHasher = quantumHashing.QuantumHash()
+        QMainWindow.__init__(self)
+        binaryValidator = QRegularExpressionValidator(QRegularExpression("[0-1]*"))
+
+        self.setMinimumSize(600, 460)
+        self.setMaximumSize(600, 460)
+        self.setWindowTitle("Kwantowo inspirowana funkcja skrótu")
+
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+        layout = QGridLayout(centralWidget)
+
+        self.inputWordLabel = QLabel(self)
+        self.inputWordLabel.setText("Binarne słowo wejściowe:")
+        layout.addWidget(self.inputWordLabel, 0, 0, 1, 1)
+
+        self.inputWordLine = QLineEdit(self)
+        self.inputWordLine.setValidator(binaryValidator)
+        layout.addWidget(self.inputWordLine, 1, 0, 1, 1)
+
+        self.controlQubitNumberLabel = QLabel(self)
+        self.controlQubitNumberLabel.setText("Liczba kubitów:")
+        self.controlQubitNumberLabel.adjustSize()
+        layout.addWidget(self.controlQubitNumberLabel, 2, 0, 1, 1)
+
+        self.inputControlQubitNumber = QSpinBox(self)
+        self.inputControlQubitNumber.setMinimum(1)
+        self.inputControlQubitNumber.setMaximum(4)
+        self.inputControlQubitNumber.valueChanged.connect(self.adjustTableRowCount)
+        layout.addWidget(self.inputControlQubitNumber, 3, 0, 1, 1)
+
+        self.paramTable = QTableWidget(self)
+        self.paramTable.setColumnCount(1)
+        self.paramTable.setRowCount(2)
+        self.paramTable.setHorizontalHeaderLabels(['Wartość współczynnika'])
+        self.paramTable.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
+        self.paramTable.setItemDelegate(TableInputDelegate())
+        layout.addWidget(self.paramTable, 5, 0, 3, 1)
+
+        self.showHistogramButton = QPushButton(self)
+        self.showHistogramButton.setText("Wyświetl histogram")
+        self.showHistogramButton.clicked.connect(self.showHistogramButtonClicked)
+        layout.addWidget(self.showHistogramButton, 0, 1, 1, 1)
+
+        self.showStateVectorButton = QPushButton(self)
+        self.showStateVectorButton.setText("Wyświetl wektor stanów")
+        self.showStateVectorButton.clicked.connect(self.showStateVectorButtonClicked)
+        layout.addWidget(self.showStateVectorButton, 1, 1, 1, 1)
+
+        self.showQuantumCircuitButton = QPushButton(self)
+        self.showQuantumCircuitButton.setText("Wyświetl schemat układu kwantowego")
+        self.showQuantumCircuitButton.clicked.connect(self.showQuantumCircuitButtonClicked)
+        layout.addWidget(self.showQuantumCircuitButton, 2, 1, 1, 1)
+
+        self.inputSwapTestWordLabel = QLabel(self)
+        self.inputSwapTestWordLabel.setText("Binarne słowo wejściowe do porównania \nw Swap test:")
+        layout.addWidget(self.inputSwapTestWordLabel, 3, 1, 1, 1)
+
+        self.inputSwapTestWordLine = QLineEdit(self)
+        self.inputSwapTestWordLine.setValidator(binaryValidator)
+        layout.addWidget(self.inputSwapTestWordLine, 4, 1, 1, 1)
+
+        self.swapTestButton = QPushButton(self)
+        self.swapTestButton.setText("Wykonaj Swap test")
+        self.swapTestButton.clicked.connect(self.swapTestButtonClicked)
+        layout.addWidget(self.swapTestButton, 5, 1, 1, 1)
+
+        self.authorsLabel = QLabel(self)
+        self.authorsLabel.setText("Autorzy: Komoszyński Łukasz, Ulaski Wojciech, Zadrożny Bartosz")
+        layout.addWidget(self.authorsLabel, 8, 0, 1, 2)
+
+    def processTable(self):
+        k = []
+        for row in range(self.paramTable.rowCount()):
+            item = self.paramTable.item(row, 0)
+            text = item.text()
+            k.append(int(text))
+
+        return k
+
+    def setupQuantumHasher(self):
+        self.quantumHasher.msg = [int(d) for d in self.inputWordLine.text()]
+        self.quantumHasher.control_qubits_num = self.inputControlQubitNumber.value()
+        self.quantumHasher.k = self.processTable()
+
+    def swapTestButtonClicked(self):
+        print(self.processTable())
+        print("swap test button clicked")
+
+    def showQuantumCircuitButtonClicked(self):
+        self.setupQuantumHasher()
+        self.quantumHasher.show_circuit()
+
+    def showHistogramButtonClicked(self):
+        self.setupQuantumHasher()
+        self.quantumHasher.show_histogram()
+
+    def showStateVectorButtonClicked(self):
+        print("show state vector button clicked")
+
+    def adjustTableRowCount(self, qubitNumber):
+        self.paramTable.setRowCount(2**qubitNumber)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setApplicationName("Kwantowo inspirowana funkcja skrótu")
+
+    mainWin = MainWindow()
+    mainWin.show()
+
+    app.exec()
--- quantumHashing.py
+++ quantumHashing.py
@@ -6,85 +6,96 @@ import math
 from qiskit import Aer, execute
 
 class QuantumHash:
-	def __init__(self, backend=Aer.get_backend('qasm_simulator'), msg=[], qubits_num=0):
-		self.backend = backend
-		self._msg = msg
-		self._qubits_num = qubits_num
-		self.circuit = QuantumCircuit(self.qubits_num)
-		self._k = []
-		self._constructed_circuit = True
-		self._result = None
+    def __init__(self, backend=Aer.get_backend('qasm_simulator'), msg=[], qubits_num=0):
+        self.backend = backend
+        self._msg = msg
+        self._qubits_num = qubits_num
+        self.circuit = QuantumCircuit(self.qubits_num)
+        self._k = []
+        self._constructed_circuit = True
+        self._result = None
 
-	def get_result(self, recalculate=False):
-		if recalculate or not self.constructed_circuit or self._result is None:
-			self.recalculate_result()
-		return self._result
+    def get_result(self, recalculate=False):
+        if recalculate or not self.constructed_circuit or self._result is None:
+            self.recalculate_result()
+        return self._result
 
-	def recalculate_result(self, shots=1000):
-		if not self.constructed_circuit:
-			self.generate_circuit()
-		self._result = execute(self.circuit.measure_all(inplace=False), self.backend, shots=shots).result()
+    def recalculate_result(self, shots=1000):
+        if not self.constructed_circuit:
+            self.generate_circuit()
+        self._result = execute(self.circuit.measure_all(inplace=False), self.backend, shots=shots).result()
 
-	def generate_circuit(self):
-		self.circuit = QuantumCircuit(self.qubits_num)
-		control_qubits_num = self.control_qubits_num
-		N = 2 ** control_qubits_num
-		bit = 1
-		self.circuit.h(range(0, control_qubits_num))
+    def generate_circuit(self):
+        self.circuit = QuantumCircuit(self.qubits_num)
+        control_qubits_num = self.control_qubits_num
+        N = 2 ** control_qubits_num
+        bit = 1
+        self.circuit.h(range(0, control_qubits_num))
 
-		for i in range(len(self.msg)):
-			if self.msg[i]:
-				for j in range(N):
-					gate = RYGate((4 * math.pi * bit * self.k[j]) / N).control(control_qubits_num, ctrl_state=j)
-					self.circuit.append(gate, range(self.qubits_num))
-			bit *= 2
-		self.result = None
-		self.constructed_circuit = True
-		self._measure_circuit = self.circuit.measure_all(inplace=False)
+        for i in range(len(self.msg)):
+            if self.msg[i]:
+                for j in range(N):
+                    gate = RYGate((4 * math.pi * bit * self.k[j]) / N).control(control_qubits_num, ctrl_state=j)
+                    self.circuit.append(gate, range(self.qubits_num))
+            bit *= 2
+        self.result = None
+        self.constructed_circuit = True
+        self._measure_circuit = self.circuit.measure_all(inplace=False)
 
-	def show_circuit(self, output='mpl'):
-		if not self.constructed_circuit:
-			self.generate_circuit()
-		self.circuit.draw(output=output)
-		plt.show()
+    def show_circuit(self, output='mpl'):
+        if not self.constructed_circuit:
+            self.generate_circuit()
+        self.circuit.draw(output=output)
+        plt.show()
 
-	def show_histogram(self):
-		res = self.get_result().get_counts(self.circuit.measure_all(inplace=False))
-		plot_histogram(res, title='Hash histogram') 
-		plt.show()
+    def show_histogram(self):
+        res = self.get_result().get_counts(self.circuit.measure_all(inplace=False))
+        plot_histogram(res, title='Hash histogram')
+        plt.show()
 
-	@property
-	def msg(self):
-		return self._msg
-	@msg.setter
-	def msg(self, value):
-		self._msg = value
+    @property
+    def msg(self):
+        return self._msg
 
-	@property
-	def control_qubits_num(self):
-		return self._qubits_num - 1
+    @msg.setter
+    def msg(self, value):
+        self._msg = value
 
-	@property
-	def qubits_num(self):
-		return self._qubits_num
+    @property
+    def control_qubits_num(self):
+        return self._qubits_num - 1
 
-	@qubits_num.setter
-	def qubits_num(self, value):
-		self._qubits_num = value
-		self.k = [i for i in range(2 ** self.control_qubits_num)] # replace with adding zeros and removing last elements
-		self.constructed_circuit = False
+    @property
+    def qubits_num(self):
+        return self._qubits_num
 
-	@property
-	def k(self):
-		return self._k
-	@k.setter
-	def k(self, value):
-		self._k = value
-		self.constructed_circuit = False
+    @control_qubits_num.setter
+    def control_qubits_num(self, value):
+        self.qubits_num = value + 1
 
-	def set_single_k(self, new_k_val, i):
-		self.k[i] = new_k_val
-		self.constructed_circuit = False
+    @qubits_num.setter
+    def qubits_num(self, value):
+        self._qubits_num = value
+        self.k = [i for i in
+                  range(2 ** self.control_qubits_num)]  # replace with adding zeros and removing last elements
+        self.constructed_circuit = False
+
+    @property
+    def k(self):
+        return self._k
+
+    @k.setter
+    def k(self, value):
+        self._k = value
+        self.constructed_circuit = False
+
+    def set_single_k(self, new_k_val, i):
+        self.k[i] = new_k_val
+        self.constructed_circuit = False
+
+
+
+
 '''
 a = QuantumHash()
 a.qubits_num = 4
@@ -92,5 +103,3 @@ a.msg = [0, 1, 0]
 a.show_circuit()
 a.show_histogram() # it will probably segfault if you try it without show_circuit()
 '''
-
-
